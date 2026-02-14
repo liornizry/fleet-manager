99a08c908b6abf1616f71d98458516afa5bf9b0e
liornizry <37239792+liornizry@users.noreply.github.com>
2026-02-15 01:56:01 +0200

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,11 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Firebase configuration
-// החלף את הערכים האלה בקונפיגורציה שלך מ-Firebase Console
-// https://console.firebase.google.com/
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyCHWrs4PFFV4h7Tfu4oMAEdQbQduzHc2xQ",
   authDomain: "carmannegement501-7eabe.firebaseapp.com",
@@ -16,8 +18,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-
-// Initialize Firestore
-export const db = getFirestore(app);
-
-export default app;
+const analytics = getAnalytics(app);
